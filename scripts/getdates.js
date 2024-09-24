ca8a573ed84db3a0a50092a3704d1080aad12c94
Drizzlemax <desmondchibanda28@gmail.com>
2024-09-24 15:42:03 +0200

    new file:   images/profile.jpg 	new file:   images/zimbabwe-flag.jpg 	modified:   index.html 	modified:   scripts/getdates.js 	modified:   styles/base.css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,11 @@
-// getdates.js
+// Get the current year for the footer
 document.addEventListener("DOMContentLoaded", function () {
     const yearSpan = document.getElementById('currentyear');
     const currentYear = new Date().getFullYear();
     yearSpan.textContent = currentYear;
+
+    // Last Modified Date
+    const lastModifiedSpan = document.getElementById('lastModified');
+    const lastModifiedDate = document.lastModified;
+    lastModifiedSpan.textContent = lastModifiedDate;
 });
